d93b43ce8bf0be4906474b85bebb92430b6ba590
coutotyler <couto.tyler@gmail.com>
2015-10-09 17:25:47 -0700

    Added option parsing. Added help funtion. Functionalized everything. Added check if already downloaded function. Added archiving function.

--- bingdesktop.sh
+++ bingdesktop.sh
@@ -1,48 +1,111 @@
 #!/bin/bash
-
-# ToDo: 
-# Add a revert option: Reverts to the previous day's image (If you don't
-# like today's)
-# Check the timestamp of the current picture. If it's from today, exit. 
-
-#export DISPLAY=:0
+#
 
 fileName=$(echo "wallpaper_"$(date --rfc-3339 seconds)".jpg" | sed -e "s/ /T/g")
-saveDir="/home/tyler/Pictures/wallpaper/"
-logFile="bingDesktop.log"
-logDir="/home/tyler/Documents/logs/"
-timeOut=120
+saveDir="/home/tyler/Pictures/wellpaper/"
+timeOut=60 #seconds
+debug="/dev/null" 
+force=0
 
-if [ ! -d $saveDir ]; then
-	mkdir -p $saveDir
-fi
+show_help() {
+	cat << EOF
+Download daily image from bing and set it as the desktop background.
 
-if [ ! -d $logDir ]; then
-	mkdir -p $logDir
-fi
+Usage:
+	$0
+Options:
+	-h --help    --> Show this help
+	-d --debug   --> Show debugging output
+	-f --force   --> Force download and setting of file
+	-a --archive --> Archive this month's pictures 
+EOF
+}
 
-#if [ ! -a $logDir$logFile ]; then
-#	touch $logDir$logFile
-#fi
-
-# Setup DBUS environment variables
-PID=$(pgrep gnome-session)
-dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)
-export DBUS_SESSION_BUS_ADDRESS=$dbus
-
-# Check network connection
-startTime=$SECONDS
-until ping -q -c 1 "www.bing.com" >> $logDir$logFile 2>&1; do 
-	if [[ $(($SECONDS - $startTime)) -ge $timeOut ]]; then
-		echo "Can't access www.bing.com"
+main() {
+	if [[ $force == 0 ]]; then check_current; fi
+	if [ ! -d $saveDir ]; then mkdir -p $saveDir; fi
+	setup_dbus
+	check_connection
+	download_and_set
+}
+	
+setup_dbus() {
+	PID=$(pgrep gnome-session)
+	dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/$PID/environ | cut -d= -f2-)
+	export DBUS_SESSION_BUS_ADDRESS=$dbus
+}
+	
+check_connection() {
+	startTime=$SECONDS
+	until ping -q -c 1 "www.bing.com" > $debug 2>&1; do 
+		if [[ $(($SECONDS - $startTime)) -ge $timeOut ]]; then
+			logger -i -t bingdesktop -p syslog.err "Can't access www.bing.com"
+			exit 1
+		fi
+	done
+}
+	
+download_and_set() {
+	if wget -t 20 --waitretry=1 --retry-connrefused "www.bing.com"$(wget -qO- www.bing.com | grep -o "url:'.*[.]jpg',id" | cut -d"'" -f2 ) -O $saveDir$fileName > $debug 2>&1; then
+		gsettings set org.gnome.desktop.background picture-uri "file://$saveDir$fileName" 
+	else
+		rm $saveDir$fileName
+		logger -i -t bingdesktop -p syslog.err 'Download failed' 
 		exit 1
 	fi
-done
+}
 
-# Download picture and set background
-if wget -t 20 --waitretry=1 --retry-connrefused "www.bing.com"$(wget -qO- www.bing.com | grep -o "url:'.*[.]jpg',id" | cut -d"'" -f2 ) -O /home/tyler/Pictures/wallpaper/"$fileName" 2>>$logDir$logFile; then
-	gsettings set org.gnome.desktop.background picture-uri "file://$saveDir$fileName" 2>>$logDir$logFile
-else
-	rm $saveDir$fileName
-	echo 'Download failed.' 
-fi
+check_current() {
+	last=`ls -1 $saveDir | tail -n 1 | sed 's/wallpaper_//' | sed 's/T.*//'`
+	if [[ $last == `date +%F` ]]; then 
+		echo "Already downloaded today's image. Exiting." > $debug
+		exit
+	fi
+}
+
+archive() {
+	files=$(ls -1 "$saveDir"*.jpg)
+	i=0
+	arDir="$saveDir$(date +%B)-$(date +%g)"
+	mkdir $arDir
+	for x in $files; do
+		y=$(echo $x | sed 's/wallpaper_//' | sed 's/T.*//' | cut -d '-' -f2)
+		if [[ $y == `date +%m` ]]; then 
+			arFiles[$i]=$x
+			i=$((i+1))
+		fi
+	done
+	for x in ${arFiles[*]}; do
+		mv $x $arDir
+	done
+	tar cjf "$arDir".tar.bz2 $arDir 2>/dev/null
+}
+
+# Parse options
+#
+while [[ $# -ge 1 ]]; do
+	key="$1"
+	case $key in
+		-h|--help)
+			show_help
+			exit
+			;; 
+		-d|--debug)
+			debug=`tty`
+			main
+			;;
+		-f|--force)
+			force=1
+			main
+			;;
+		-a|--archive)
+			archive
+			exit
+			;;
+		*)
+			show_help
+			exit
+			;;
+	esac
+	shift
+done
